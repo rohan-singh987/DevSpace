9c3e2f9033a6ab4953607806cfeb46f933342fb9
Rohan Singh <rohannsingh98765@gmail.com>
2023-05-04 22:12:01 +0530

    data and homePage
    Signed-off-by: Rohan Singh <rohannsingh98765@gmail.com>

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 
+import BlogItem from '../components/BlogItem';
+import Search from '../components/Search';
+import { data } from '../config/data';
+
 const Home = () => {
   return (
     <div>
-        
+        <Search />
+
+        <BlogItem blogs={data} />
     </div>
   )
 }
